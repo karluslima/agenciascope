f8076abd6468a55371b1eb8d4b3c6f721d4de58a
karluslima <karlus@live.com>
2024-08-19 23:56:04 -0300

    feat: change header font

--- app/layouts/partials/Header.tsx
+++ app/layouts/partials/Header.tsx
@@ -1,9 +1,9 @@
 import Button from "@/app/components/Button/button";
 import NavBar from "@/app/components/Navbar/Navbar";
-import { Nunito } from "next/font/google";
+import { Roboto, Roboto_Condensed } from "next/font/google";
 
-const nunito = Nunito({
-  weight: ['400', '600'],
+const roboto_condensed = Roboto({
+  weight: ['400', '700'],
   subsets: ["latin"],
   display: 'swap'
 });
@@ -15,7 +15,7 @@ export default function Header() {
       <div className="bg-overlay bg-black/[.3] w-full h-screen absolute"></div>
       <NavBar />
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 items-center flex flex-col">
-        <h1 className={nunito.className + " text-white font-semibold lg:text-7xl md:text-5xl text-3xl text-center mb-6"}>
+        <h1 className={roboto_condensed.className + " text-white font-semibold lg:text-7xl md:text-5xl text-3xl text-center mb-6"}>
           Soluções digitais <br />resultados reais
         </h1>
         <Button buttonType="transparent" label="O que fazemos?" />
