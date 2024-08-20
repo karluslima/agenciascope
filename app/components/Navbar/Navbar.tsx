d638bd84d7aca2dfca9f4c9ba6f9d96b00e75c5f
karluslima <karlus@live.com>
2024-08-19 23:26:10 -0300

    feat: change navbar z-index

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import MainMenu from '../MainMenu/MainMenu'
 
 export default function NavBar() {
   return (
-    <div className="p-4 flex flex-col items-center border-b-[1px] border-b-white/30 fixed w-full backdrop-blur">
+    <div className="p-4 flex z-50 flex-col items-center border-b-[1px] border-b-white/30 fixed w-full backdrop-blur">
       <Logo />
       <MainMenu />
     </div>
