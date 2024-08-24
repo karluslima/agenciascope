ffee2d1fa277d720e89f8f5d5f20f281512975ef
karluslima <karlus@live.com>
2024-08-24 15:58:13 -0300

    fix: title font weight

--- app/components/Titles/Titles.tsx
+++ app/components/Titles/Titles.tsx
@@ -5,7 +5,7 @@ type SectionTitle = {
 }
 export default function Titles( props: SectionTitle ) {
   return (
-    <h2 className={titillium.className +" section-title w-full text-center sm:w-auto font-bold inline-block py-3 text-4xl md:text-5xl"}>
+    <h2 className={titillium.className +" section-title w-full text-center sm:w-auto inline-block py-3 text-4xl md:text-5xl"}>
       {props.title}
     </h2>
   )
