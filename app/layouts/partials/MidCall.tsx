2a32235f829a1c95b8e922639f69d3893d8fc3f1
karluslima <karlus@live.com>
2024-08-27 19:19:45 -0300

    fix: font size in mobile

--- app/layouts/partials/MidCall.tsx
+++ app/layouts/partials/MidCall.tsx
@@ -3,7 +3,7 @@ import { oswald } from "@/app/lib/fonts";
 export default function MidCall() {
   return (
     <section id="midcall" className="relative flex items-center h-12 h-40 sm:h-96">
-      <h2 className={ oswald.className + " z-20 m-auto text-center text-white text-3xl z-30 font-bold"}>
+      <h2 className={oswald.className + " z-20 m-auto text-center text-white text-base sm:text-3xl z-30 font-bold"}>
         Soluções digitais sob medida. Resultados que fazem a diferença. <br/>
         <small className="font-normal">A agência digital que entende o <span className="midhighlight">seu negócio.</span></small>
       </h2>
