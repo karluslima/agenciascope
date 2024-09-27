f33d6d6e4e268d6ec5edfeb5a38b52144cb95d46
karluslima <karlus@live.com>
2024-09-27 17:36:40 -0300

    feat: first footer

--- app/components/Copyright/copyright.tsx
+++ app/components/Copyright/copyright.tsx
@@ -4,7 +4,7 @@ export default function Copyright() {
   const year = new Date().getFullYear();
   return (
     <div className="copyright bg-[#15202b]">
-      <div className={`container m-auto text-white border-t-[1px] border-slate-50 py-10 text-center ${titillium.className}`}>
+      <div className={`container m-auto text-white border-t-[1px] border-slate-700 py-10 text-center ${titillium.className}`}>
         © {year} Todos os direitos reservados.
       </div>
     </div>
--- app/layouts/partials/Footer.tsx
+++ app/layouts/partials/Footer.tsx
@@ -4,7 +4,11 @@ import Copyright from "@/app/components/Copyright/copyright";
 export default function Footer() {
   return (
     <footer className="bg-black h-[320px]">
-      <ContactForm />
+      <div className="contact-form flex flex-col justify-center items-center p-20 bg-[#15202b]">
+        <span className="text-white text-3xl font-bold"> Vamos conversar? </span>
+        <a className="text-white" href="mailto:falecom@agenciascope.com.br"> falecom@agenciascope.com.br</a>
+      </div>
+      {/* <ContactForm /> */}
       <Copyright />
     </footer>
   )
